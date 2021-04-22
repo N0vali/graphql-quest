a195fe5e02376113413cce136d17908a287dc5f6
Alicia Pilar <aliciapilar@macbook-air-de-alicia.home>
2021-04-22 10:08:12 +0200

    graphql quest 1

--- src/index.js
+++ src/index.js
@@ -1,13 +1,63 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { ApolloClient,ApolloProvider, InMemoryCache, useQuery, gql } from '@apollo/client';
 
+
+const EXCHANGE_RATES = gql`
+  query GetExchangeRates {
+    rates(currency: "USD") {
+      currency
+      rate
+    }
+  }
+`;
+
+function ExchangeRates() {
+  const { loading, error, data } = useQuery(EXCHANGE_RATES);
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error :(</p>;
+
+  return data.rates.map(({ currency, rate }) => (
+    <div key={currency}>
+      <p>
+        {currency}: {rate}
+      </p>
+    </div>
+  ));
+}
+const client = new ApolloClient({
+  uri: 'https://48p1r2roz4.sse.codesandbox.io',
+  cache: new InMemoryCache()
+});
+
+client
+  .query({
+    query: gql`
+      query GetRates {
+        rates(currency: "USD") {
+          currency
+        }
+      }
+    `
+  })
+  .then(result => console.log(result));
+  
+  
+  function App() {
+    return (
+      <div>
+        <h2>My first Apollo app 🚀</h2>
+      </div>
+    );
+  }
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <ApolloProvider client={client}>
+  <App />
+  <ExchangeRates/>
+  </ApolloProvider>,
   document.getElementById('root')
 );
 
